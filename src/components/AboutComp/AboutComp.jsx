27ac4d130eda7baf107153a479138711931a1e56
Mario Fernandez <marioteleco@outlook.com>
2024-04-04 10:08:00 +0200

    New info content were added

--- src/components/AboutComp/AboutComp.jsx
+++ src/components/AboutComp/AboutComp.jsx
@@ -1,50 +1,63 @@
 import styles from "./AboutComp.module.css";
 
 const AboutComp = () => {
+  return (
+    <>
+      <div className={styles.aboutMain}>
+        <div className={styles.aboutSection}>
+          <div className={styles.imgAboutSection}>
+            <img src="../../../img/example.jpg" alt="foto empresa" />
+          </div>
 
-    return (
-        <>
-            <div className={styles.aboutMain}>
+          <div className={styles.textoAboutSection}>
+            <h2>Acerca de QuizMatrix</h2>
 
-                <div className={styles.aboutSection}>
+            <p>
+              En nuestra emocionante travesía en el desarrollo de QuizMatrix,
+              nos encontramos inmersos en la exploración y adopción de una
+              amplia gama de tecnologías para impulsar tanto el Frontend como el
+              Backend hacia la excelencia. En la esfera del Frontend, nos
+              destacamos por el uso de React.js, una potente biblioteca de
+              JavaScript, en combinación con Yarn para la gestión de paquetes y
+              Vite para la administración eficiente del proyecto React. Además,
+              hemos integrado diversas bibliotecas de terceros, como Axios, para
+              facilitar las solicitudes HTTP al servidor, asegurando así una
+              experiencia de usuario fluida y eficaz.
+            </p>
 
-                    <div className={styles.imgAboutSection}>
-                        <img src="../../../img/example.jpg" alt="foto empresa" />
-                    </div>
+            <p>
+              Por otro lado, en el Backend, estamos inmersos en la exploración
+              de diferentes versiones para potenciar nuestras capacidades y
+              ampliar nuestro conocimiento en el mundo del desarrollo web con
+              Java. La primera versión de nuestro backend sigue el enfoque
+              tradicional, utilizando Servlets, mientras que otra implementa el
+              patrón de diseño MVC con DAO o Hibernate para la interacción con
+              la base de datos. Nuestro objetivo final es culminar con una
+              versión robusta y eficiente utilizando Spring Boot, una plataforma
+              que ofrece una solución integral y altamente escalable.
+            </p>
 
-                    <div className={styles.textoAboutSection}>
+            <p>
+              Cada una de estas etapas y versiones se han diseñado
+              estratégicamente para proporcionarnos las habilidades y el
+              conocimiento necesario no solo para este proyecto en particular,
+              sino también para adaptarnos fluidamente a diversos entornos de
+              trabajo que utilicen Java como base tecnológica.
+            </p>
 
-                        <h2>Acerca de QuizMatrix</h2>
-
-                        <p>
-                            Desarrollando una plataforma para la realización de tests online .
-                            Las tecnologías que se están usando tanto en Frontend como en
-                            Backend son React, Javascript, Css, HTML, Java EE, Java Servelets,
-                            JSP etc. Además de todas estas tecnologías se está usando como
-                            control de versiones Git y trabajando en remoto con Github.
-                        </p>
-
-                        <p>
-                            El objetivo del proyecto es el poder integrar todo el potencial de
-                            Java con Spring Boot para gestionar todas las peticiones del Front
-                            End. En este caso nuestro JSP tendrá que gestionar mediante Servlets
-                            toda la información que necesita el Front, como cargar nuestros
-                            tests HTML desde el Back, registros de usuarios, validador de
-                            respuestas, Login, sesiones, roles etc.
-                        </p>
-
-                        <p className={styles.parrafo3}>
-                            Para la realización de este proyecto están colaborando varios
-                            compañeros que brevemente serán añadidos.
-                        </p>
-
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+            <p className={styles.parrafo3}>
+              Este emocionante proyecto es el resultado de la colaboración entre
+              varios compañeros del curso de Aplicaciones Web con Java/Spring en
+              Core Network. Juntos, estamos trazando un camino hacia la
+              excelencia en el desarrollo de aplicaciones Java, aprovechando al
+              máximo nuestras habilidades y conocimientos para alcanzar nuevos
+              horizontes en el mundo del desarrollo de software.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutComp;
-
-
--- src/components/TestComp/TextComp.jsx
+++ src/components/TestComp/TextComp.jsx
@@ -1,43 +1,67 @@
 import styles from "./TextComp.module.css";
 
 const TextComp = () => {
+  return (
+    <>
+      <div className={styles.testInfo}>
+        <h1 className={styles.titulo}>QuizMatrix</h1>
 
-    return (
+        <p className={styles.parrafo}>
+          Nuestra plataforma se especializa en ofrecer una amplia variedad de
+          tests en línea dedicados exclusivamente a la programación en Java,
+          JavaScript, HTML y CSS. Estos tests están diseñados para evaluar y
+          mejorar tus habilidades en estos lenguajes y tecnologías fundamentales
+          para el desarrollo web y de software.
+        </p>
+        <p className={styles.parrafo}>
+          Cada test ha sido minuciosamente creado por expertos en la materia,
+          garantizando la relevancia y la calidad de los contenidos. Desde
+          preguntas básicas hasta desafíos más avanzados, nuestra plataforma
+          ofrece una amplia gama de preguntas para ayudarte a fortalecer tus
+          conocimientos y habilidades en programación.
+        </p>
+        <p className={styles.parrafo}>
+          Además de proporcionar tests desafiantes, también ofrecemos
+          retroalimentación detallada y análisis de resultados para que puedas
+          identificar áreas de mejora y seguir progresando en tu aprendizaje. Ya
+          sea que estés preparándote para una entrevista técnica, ampliando tus
+          habilidades de programación, o simplemente disfrutando del desafío de
+          resolver problemas de código, estamos aquí para ayudarte en tu camino
+          hacia el dominio de la programación en Java, JavaScript, HTML y CSS.
+        </p>
 
-        <>
+        <h2 className={styles.subTitulo}>Tipos de pruebas:</h2>
 
-            <div className={styles.testInfo}>
+        <ul className={styles.listado}>
+          <li className={styles.parrafoTecnologia}>
+            <strong>HTML:</strong> Evalúa tu comprensión de la estructura y
+            semántica web.
+          </li>
 
-                <h1 className={styles.titulo}>QuizMatrix</h1>
+          <li className={styles.parrafoTecnologia}>
+            <strong>CSS:</strong> Evalúa tu dominio en técnicas de estilo y
+            diseño.
+          </li>
 
-                <p className={styles.parrafo}>
-                    Nuestra plataforma ofrece una variedad de pruebas en línea para evaluar y mejorar tu conocimiento en diferentes áreas.
-                </p>
+          <li className={styles.parrafoTecnologia}>
+            <strong>JavaScript:</strong> Pon a prueba tus habilidades en
+            resolución de problemas y codificación dinámica.
+          </li>
 
-                <h2 className={styles.subTitulo}>Tipos de pruebas:</h2>
+          <li className={styles.parrafoTecnologia}>
+            <strong>Java:</strong> Descubre tus capacidades en programación
+            orientada a objetos y desarrollo de aplicaciones robustas y
+            escalables.
+          </li>
+        </ul>
 
-                <ul className={styles.listado}>
-
-                    <li className={styles.parrafoTecnologia}>
-                        <strong>HTML: </strong>Evalúa tu comprensión de la estructura y semántica web.
-                    </li>
-
-                    <li className={styles.parrafoTecnologia}>
-                        <strong>CSS: </strong>Evalúa tus habilidades en técnicas de estilo y diseño.
-                    </li>
-
-                    <li className={styles.parrafoTecnologia}>
-                        <strong>JavaScript: </strong>Desafía tus habilidades de resolución de problemas y codificación.
-                    </li>
-
-                </ul>
-
-                <p className={styles.texto}>Elige una prueba que se adapte a tus habilidades y empieza a mejorar hoy mismo!</p>
-
-            </div>
-
-        </>
-    );
+        <p className={styles.texto}>
+          Elige una prueba que se adapte a tus habilidades y empieza a mejorar
+          hoy mismo!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default TextComp;
