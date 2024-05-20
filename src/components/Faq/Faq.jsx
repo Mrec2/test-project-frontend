b572989c5f120824192ae26b8219bb41fa6841a4
Cochises <soycochises@gmail.com>
2024-05-20 21:46:23 +0200

    Modification of component styles frequently asked questions and terms and conditions

--- src/components/Faq/Faq.jsx
+++ src/components/Faq/Faq.jsx
@@ -1,121 +1,173 @@
 import styles from "./Faq.module.css";
 
 const Faq = () => {
-  return (
-    <div className={styles.faq}>
-      <h2 className={styles.faqH2}>Frequently Asked Questions</h2>
-      <main className={styles.faqMain}>
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>
-            Pregunta 1: ¿Qué temas de programación están cubiertos en la
-            plataforma?
-          </h2>
-          <p className={styles.faqP}>
-            La plataforma cubre una amplia variedad de temas de programación,
-            incluyendo Java, CSS, HTML y JavaScript. Puedes encontrar quizzes
-            relacionados con estos lenguajes de programación y más.
-          </p>
-        </section>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>Pregunta 2: ¿Cómo puedo acceder a los quizzes?</h2>
-          <p className={styles.faqP}>
-            Para acceder a los quizzes, primero necesitas registrarte en nuestra
-            plataforma. Una vez registrado, puedes explorar y seleccionar los
-            quizzes que te interesen para empezar a resolverlos.
-          </p>
-        </section>
+    return (
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>Pregunta 3: ¿Son los quizzes gratuitos?</h2>
-          <p className={styles.faqP}>
-            Sí, todos los quizzes disponibles en nuestra plataforma son
-            gratuitos para los usuarios registrados. No hay ningún cargo por
-            resolver quizzes o acceder a los recursos educativos.
-          </p>
-        </section>
+        <div className={styles.container}>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>Pregunta 4: ¿Hay límite de intentos para resolver un quiz?</h2>
-          <p className={styles.faqP}>
-            No, no hay límite de intentos para resolver un quiz. Puedes intentar
-            resolver un quiz tantas veces como desees para mejorar tu
-            comprensión y puntaje.
-          </p>
-        </section>
+            <h2 className={styles.tituloPrincipal}>Preguntas frecuentes</h2>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>
-            Pregunta 5: ¿Puedo crear y compartir mis propios quizzes en la
-            plataforma?
-          </h2>
-          <p className={styles.faqP}>
-            Actualmente, la función para crear y compartir tus propios quizzes
-            no está disponible. Sin embargo, estamos trabajando en agregar esta
-            característica en futuras actualizaciones de la plataforma.
-          </p>
-        </section>
+            <main className={styles.containerPreguntas}>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>Pregunta 6: ¿Cómo puedo contactar al equipo de soporte?</h2>
-          <p className={styles.faqP}>
-            Si tienes alguna pregunta, problema técnico o sugerencia, puedes
-            ponerte en contacto con nuestro equipo de soporte enviando un correo
-            electrónico a support@plataformaquizzes.com. Estaremos encantados de
-            ayudarte.
-          </p>
-        </section>
+                <section className={styles.containerPregunta}>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>Pregunta 7: ¿Qué nivel de dificultad tienen los quizzes?</h2>
-          <p className={styles.faqP}>
-            Los quizzes están diseñados para adaptarse a diferentes niveles de
-            habilidad, desde principiante hasta avanzado. Puedes encontrar
-            quizzes de diferentes niveles de dificultad para desafiar y mejorar
-            tus habilidades de programación.
-          </p>
-        </section>
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Qué temas de programación están cubiertos en la plataforma?
+                    </h2>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>
-            Pregunta 8: ¿Se proporcionan respuestas explicativas después de
-            resolver un quiz?
-          </h2>
-          <p className={styles.faqP}>
-            Sí, después de resolver un quiz, se te proporcionarán respuestas
-            detalladas y explicaciones para cada pregunta. Esto te ayudará a
-            comprender los conceptos y mejorar tus habilidades de programación.
-          </p>
-        </section>
+                    <p className={styles.parrafoPregunta}>
+                        La plataforma cubre una amplia variedad de temas de programación,
+                        incluyendo Java, CSS, HTML y JavaScript. Puedes encontrar quizzes
+                        relacionados con estos lenguajes de programación y más.
+                    </p>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>
-            Pregunta 9: ¿Es necesario tener conocimientos previos de
-            programación para utilizar la plataforma?
-          </h2>
-          <p className={styles.faqP}>
-            No, nuestra plataforma está diseñada para usuarios de todos los
-            niveles, desde principiantes hasta expertos. Incluso si eres nuevo
-            en la programación, encontrarás quizzes y recursos que te ayudarán a
-            aprender y mejorar tus habilidades.
-          </p>
-        </section>
+                </section>
 
-        <section className={styles.faqSection}>
-          <h2 className={styles.faqH2}>
-            Pregunta 10: ¿Se proporciona retroalimentación sobre el desempeño en
-            los quizzes?
-          </h2>
-          <p className={styles.faqP}>
-            Sí, al completar un quiz, recibirás retroalimentación instantánea
-            sobre tu desempeño, incluyendo tu puntaje y las áreas en las que
-            podrías mejorar. Esta retroalimentación te ayudará a identificar tus
-            fortalezas y debilidades en la programación.
-          </p>
-        </section>
-      </main>
-    </div>
-  );
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Cómo puedo acceder a los quizzes?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Para acceder a los quizzes, primero necesitas registrarte en nuestra
+                        plataforma. Una vez registrado, puedes explorar y seleccionar los
+                        quizzes que te interesen para empezar a resolverlos.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Son los quizzes gratuitos?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Sí, todos los quizzes disponibles en nuestra plataforma son
+                        gratuitos para los usuarios registrados. No hay ningún cargo por
+                        resolver quizzes o acceder a los recursos educativos.
+                    </p>
+
+                </section>
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Hay límite de intentos para resolver un quiz?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        No, no hay límite de intentos para resolver un quiz. Puedes intentar
+                        resolver un quiz tantas veces como desees para mejorar tu
+                        comprensión y puntaje.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Puedo crear y compartir mis propios quizzes en la plataforma?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Actualmente, la función para crear y compartir tus propios quizzes
+                        no está disponible. Sin embargo, estamos trabajando en agregar esta
+                        característica en futuras actualizaciones de la plataforma.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Cómo puedo contactar al equipo de soporte?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Si tienes alguna pregunta, problema técnico o sugerencia, puedes
+                        ponerte en contacto con nuestro equipo de soporte enviando un correo
+                        electrónico a support@plataformaquizzes.com. Estaremos encantados de
+                        ayudarte.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Qué nivel de dificultad tienen los quizzes?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Los quizzes están diseñados para adaptarse a diferentes niveles de
+                        habilidad, desde principiante hasta avanzado. Puedes encontrar
+                        quizzes de diferentes niveles de dificultad para desafiar y mejorar
+                        tus habilidades de programación.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Se proporcionan respuestas explicativas después de resolver un quiz?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Sí, después de resolver un quiz, se te proporcionarán respuestas
+                        detalladas y explicaciones para cada pregunta. Esto te ayudará a
+                        comprender los conceptos y mejorar tus habilidades de programación.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Es necesario tener conocimientos previos de programación para utilizar la plataforma?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        No, nuestra plataforma está diseñada para usuarios de todos los
+                        niveles, desde principiantes hasta expertos. Incluso si eres nuevo
+                        en la programación, encontrarás quizzes y recursos que te ayudarán a
+                        aprender y mejorar tus habilidades.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerPregunta}>
+
+                    <h2 className={styles.tituloPregunta}>
+                        ¿Se proporciona retroalimentación sobre el desempeño en los quizzes?
+                    </h2>
+
+                    <p className={styles.parrafoPregunta}>
+                        Sí, al completar un quiz, recibirás retroalimentación instantánea
+                        sobre tu desempeño, incluyendo tu puntaje y las áreas en las que
+                        podrías mejorar. Esta retroalimentación te ayudará a identificar tus
+                        fortalezas y debilidades en la programación.
+                    </p>
+
+                </section>
+
+            </main>
+
+        </div>
+
+    );
+
 };
 
 export default Faq;
--- src/pages/TermsOfService/Terms.jsx
+++ src/pages/TermsOfService/Terms.jsx
@@ -1,79 +1,101 @@
 import styles from "./Terms.module.css";
 
 const Terms = () => {
-  return (
-    <>
-      <header>
-        <h1>Términos de Servicio</h1>
-      </header>
 
-      <main>
-        <section>
-          <h2>1. Aceptación de los Términos</h2>
-          <p>
-            Al acceder o utilizar nuestra plataforma de cualquier manera,
-            aceptas cumplir estos términos de servicio. Si no estás de acuerdo
-            con alguno de los términos, te pedimos que no utilices nuestros
-            servicios.
-          </p>
-        </section>
+    return (
+        
+        <div className={styles.container}>
+            
+                <h1 className={styles.tituloPrincipal}>Términos de Servicio</h1>
 
-        <section>
-          <h2>2. Uso Aceptable</h2>
-          <p>
-            Nuestra plataforma está destinada únicamente para uso personal y no
-            comercial. Te comprometes a utilizar nuestros servicios de manera
-            ética y legal. No deberás utilizar la plataforma para cualquier
-            actividad ilegal o que viole los derechos de otros usuarios o
-            terceros.
-          </p>
-        </section>
+                <section className={styles.containerTerminos}>
 
-        <section>
-          <h2>3. Registro de Cuenta</h2>
-          <p>
-            Es posible que necesites registrarte para acceder a ciertas
-            funciones de nuestra plataforma. Debes proporcionar información
-            precisa y actualizada durante el proceso de registro. Eres
-            responsable de mantener la confidencialidad de tu cuenta y
-            contraseña.
-          </p>
-        </section>
+                    <h2 className={styles.tituloTermino}>1. Aceptación de los Términos</h2>
 
-        <section>
-          <h2>4. Propiedad Intelectual</h2>
-          <p>
-            Todos los derechos de propiedad intelectual relacionados con nuestra
-            plataforma y sus contenidos son propiedad nuestra o de nuestros
-            licenciantes. No tienes derecho a copiar, modificar, distribuir,
-            transmitir, mostrar públicamente o crear trabajos derivados de
-            ningún contenido de nuestra plataforma sin nuestro consentimiento
-            previo por escrito.
-          </p>
-        </section>
+                    <p className={styles.parrafoTermino}>
+                        Al acceder o utilizar nuestra plataforma de cualquier manera,
+                        aceptas cumplir estos términos de servicio. Si no estás de acuerdo
+                        con alguno de los términos, te pedimos que no utilices nuestros
+                        servicios.
+                    </p>
 
-        <section>
-          <h2>5. Limitación de Responsabilidad</h2>
-          <p>
-            No seremos responsables de ningún daño directo, indirecto,
-            incidental, especial, emergente o punitivo que surja del uso de
-            nuestra plataforma.
-          </p>
-        </section>
+                </section>
 
-        <section>
-          <h2>6. Modificaciones</h2>
-          <p>
-            Nos reservamos el derecho de modificar estos términos de servicio en
-            cualquier momento. Los cambios entrarán en vigencia inmediatamente
-            después de su publicación en nuestra plataforma. Es tu
-            responsabilidad revisar periódicamente estos términos para estar al
-            tanto de las modificaciones.
-          </p>
-        </section>
-      </main>
-    </>
-  );
+
+                <section className={styles.containerTerminos}>
+
+                    <h2 className={styles.tituloTermino}>2. Uso Aceptable</h2>
+
+                    <p className={styles.parrafoTermino}>
+                        Nuestra plataforma está destinada únicamente para uso personal y no
+                        comercial. Te comprometes a utilizar nuestros servicios de manera
+                        ética y legal. No deberás utilizar la plataforma para cualquier
+                        actividad ilegal o que viole los derechos de otros usuarios o
+                        terceros.
+                    </p>
+
+                </section>
+
+                <section className={styles.containerTerminos}>
+
+                    <h2 className={styles.tituloTermino}>3. Registro de Cuenta</h2>
+
+                    <p className={styles.parrafoTermino}>
+                        Es posible que necesites registrarte para acceder a ciertas
+                        funciones de nuestra plataforma. Debes proporcionar información
+                        precisa y actualizada durante el proceso de registro. Eres
+                        responsable de mantener la confidencialidad de tu cuenta y
+                        contraseña.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerTerminos}>
+
+                    <h2 className={styles.tituloTermino}>4. Propiedad Intelectual</h2>
+
+                    <p className={styles.parrafoTermino}>
+                        Todos los derechos de propiedad intelectual relacionados con nuestra
+                        plataforma y sus contenidos son propiedad nuestra o de nuestros
+                        licenciantes. No tienes derecho a copiar, modificar, distribuir,
+                        transmitir, mostrar públicamente o crear trabajos derivados de
+                        ningún contenido de nuestra plataforma sin nuestro consentimiento
+                        previo por escrito.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerTerminos}>
+
+                    <h2 className={styles.tituloTermino}>5. Limitación de Responsabilidad</h2>
+
+                    <p className={styles.parrafoTermino}>
+                        No seremos responsables de ningún daño directo, indirecto,
+                        incidental, especial, emergente o punitivo que surja del uso de
+                        nuestra plataforma.
+                    </p>
+
+                </section>
+
+
+                <section className={styles.containerTerminos}>
+
+                    <h2 className={styles.tituloTermino}>6. Modificaciones</h2>
+
+                    <p className={styles.parrafoTermino}>
+                        Nos reservamos el derecho de modificar estos términos de servicio en
+                        cualquier momento. Los cambios entrarán en vigencia inmediatamente
+                        después de su publicación en nuestra plataforma. Es tu
+                        responsabilidad revisar periódicamente estos términos para estar al
+                        tanto de las modificaciones.
+                    </p>
+
+                </section>
+
+        </div>
+    );
 };
 
 export default Terms;
