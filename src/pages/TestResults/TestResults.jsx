f271142501402332f8ca010dcea3f06b88dcb9a9
Mario Fernandez <marioteleco@outlook.com>
2024-05-23 09:43:58 +0200

    TestResults updated

--- src/pages/TestResults/TestResults.jsx
+++ src/pages/TestResults/TestResults.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import styles from "./testResults.module.css";
 
 const TestResults = () => {
   const [data, setData] = useState("");
@@ -8,15 +9,28 @@ const TestResults = () => {
     if (datosLocalStorage) {
       console.log("misResultados", JSON.parse(datosLocalStorage));
       setData(JSON.parse(datosLocalStorage));
+      //Prueba para color verde
+      // setData(25);
     }
   }, []);
 
   return (
-    <div>
-      <h2>
+    <div className={styles.testResultsContainer}>
+      <h2
+        className={styles.testTestResultsTitle}
+        style={{ color: (data / 25) * 100 > 50 ? "green" : "red" }}
+      >
         La puntuación de tu test es de <span>{data}</span> puntos sobre 25
       </h2>
-      <h2>{(data / 25) * 10}</h2>
+      <h2
+        className={styles.testResultsScore}
+        style={{ color: (data / 25) * 100 > 50 ? "green" : "red" }}
+      >
+        {(data / 25) * 100}%{" "}
+        {(data / 25) * 100 > 50
+          ? "Has superado el test"
+          : "N.M Necesitas mejorar"}
+      </h2>
     </div>
   );
 };
